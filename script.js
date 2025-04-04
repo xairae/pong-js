c8381295ad0bedc0aff04f4c30bbe8aec92d4640
Maria <98948572+xairae@users.noreply.github.com>
2025-04-05 00:04:41 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -1,140 +1,98 @@
+kaplay({
+    width: 800,
+    height: 600,
+    background: [51, 18, 48],
+});
 
-kaplay();
-
-loadSprite("snake", "https://play.kaplayjs.com/sprites/bean.png");
-loadSprite("food", "https://play.kaplayjs.com/sprites/apple.png");
-loadSprite("background", "https://i.imgur.com/mmrRkMe.jpeg");
-
-add([
-    sprite("background"), 
-    pos(-40, -200),              
-    fixed()              
+const leftPaddle = add([
+    pos(40, 0),
+    rect(20, 80),
+    outline(4, rgb(255, 255, 255)),
+    anchor("center"),
+    area(),
+    color([0, 0, 0]),
+    "paddle",
 ]);
 
-const vec = (x, y) => ({ x, y, eq: function (v) { return this.x === v.x && this.y === v.y }, clone: function () { return vec(this.x, this.y) }, add: function(v) { return vec(this.x + v.x, this.y + v.y) }, scale: function(n) { return vec(this.x * n, this.y * n) } });
-const cellSize = 20;
-const gridWidth = 15;
-const gridHeight = 15;
-let direction = vec2(1, 0); // parte andando a destra
-let snake = [];
-let food;
-let moveTimer = 0;
-let moveInterval = 0.2;
-let gameOver = false;
+const rightPaddle = add([
+    pos(width() - 40, 0),
+    rect(20, 80),
+    outline(4),
+    anchor("center"),
+    area(),
+    "paddle",
+]);
 
-// inizializza il serpente
-function initSnake() {
-    snake.forEach(s => destroy(s));
-    snake = [];
+let speedAI = 4;
+const aiAccuracy = 0.80; // 50% chance to move towards the ball
 
-    const start = vec2(5, 7);
-    const head = add([
-        sprite("snake"),
-        pos(start.scale(cellSize)),
-        area(),
-        "snakePart",
-        { dir: direction, gridPos: start.clone() }
-    ]);
-    snake.push(head);
-}
-initSnake();
-
-function spawnFood() {
-    if (food) destroy(food);
-
-    const posX = Math.floor(rand(0, gridWidth));
-    const posY = Math.floor(rand(0, gridHeight));
-    
-    // evita che il cibo appaia sulla testa
-    if (snake.some(s => s.gridPos.eq(vec2(posX, posY)))) return spawnFood();
-
-    food = add([
-        sprite("food"),
-        pos(vec2(posX, posY).scale(cellSize)),
-        area(),
-        "food",
-        { gridPos: vec2(posX, posY) }
-    ]);
-}
-spawnFood();
-
-onKeyPress("up", () => {
-    if (direction.y !== 1) direction = vec2(0, -1);
-});
-onKeyPress("down", () => {
-    if (direction.y !== -1) direction = vec2(0, 1);
-});
-onKeyPress("left", () => {
-    if (direction.x !== 1) direction = vec2(-1, 0);
-});
-onKeyPress("right", () => {
-    if (direction.x !== -1) direction = vec2(1, 0);
+// move paddles with mouse
+onUpdate(() => {
+    leftPaddle.pos.y = mousePos().y;
 });
 
-function moveSnake() {
-    const head = snake[0];
-    const newHeadPos = head.gridPos.add(direction);
+let score = 0;
 
-    // check collisione muri o se stesso
-    if (
-        newHeadPos.x < 0 || newHeadPos.x >= gridWidth ||
-        newHeadPos.y < 0 || newHeadPos.y >= gridHeight ||
-        snake.some(s => s.gridPos.eq(newHeadPos))
-    ) {
-        return gameOverScreen();
+add([
+    text(score.toString()),
+    pos(width() / 2, 40),
+    anchor("center"),
+    z(0),
+    {
+        update() {
+            this.text = score.toString();
+        },
+    },
+]);
+
+let speed = 480;
+
+const ball = add([
+    pos(center()),
+    circle(16),
+    outline(4),
+    area({ shape: new Rect(vec2(-16), 32, 32) }),
+    { vel: Vec2.fromAngle(rand(-20, 20)) },
+]);
+
+ball.onUpdate(() => {
+    ball.move(ball.vel.scale(speed));
+
+    if (ball.pos.x < 0) {
+        score = 0;
+        ball.pos = center();
+        ball.vel = Vec2.fromAngle(rand(-20, 20));
+        speed = 320;
     }
 
-    const newHead = add([
-        sprite("snake"),
-        pos(newHeadPos.scale(cellSize)),
-        area(),
-        "snakePart",
-        { dir: direction, gridPos: newHeadPos.clone() }
-    ]);
-
-    snake.unshift(newHead);
-
-    if (food && newHeadPos.eq(food.gridPos)) {
-        destroy(food);
-        spawnFood();
-    } else {
-        const tail = snake.pop();
-        destroy(tail);
+    if (ball.pos.x > width()) {
+        score++;
+        ball.pos = center();
+        ball.vel = Vec2.fromAngle(rand(-20, 20));
+        speed = 320;
     }
-}
 
-function gameOverScreen() {
-    gameOver = true;
-
-    add([
-        rect(width(), height()),
-        pos(0, 0),
-        color(0, 0, 0, 0.7),
-        fixed()
-    ]);
-
-    add([
-        text("Game Over\nPremi R per riprovare", { size: 24 }),
-        pos(center()),
-        anchor("center"),
-        fixed()
-    ]);
-}
-
-onKeyPress("r", () => {
-    if (gameOver) {
-        gameOver = false;
-        initSnake();
-        spawnFood();
+    if (ball.pos.y < 0 || ball.pos.y > height()) {
+        ball.vel.y = -ball.vel.y;
     }
+
+    if (Math.random() < aiAccuracy) {
+        if (rightPaddle.pos.y < ball.pos.y) {
+            rightPaddle.pos.y += speedAI;  // Move down
+        }
+        if (rightPaddle.pos.y > ball.pos.y) {
+            rightPaddle.pos.y -= speedAI;  // Move up
+        }
+    }
+    else {
+        const randomMove = Math.random() < 0.5 ? 1 : -1;  // Move randomly up or down
+        rightPaddle.pos.y += speedAI * randomMove;
+    }
+
 });
 
-onUpdate((dt) => {
-    if (gameOver) return;
-
-    moveTimer += dt;
-    if (moveTimer >= moveInterval) {
-        moveTimer = 0;
-        moveSnake(); // This function already handles movement
-    }
+ball.onCollide("paddle", (p) => {
+    speed += 30;
+    speedAI += 0.5;
+    ball.vel = Vec2.fromAngle(ball.pos.angle(p.pos));
 });
